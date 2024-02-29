f294abb0bf06bdee2c9dcf8d3fbabe7c6d09fc4c
Bunje Kelly <kellybunje@gmail.com>
2024-02-29 20:08:06 +0000

    annotated function

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+This module contains an annotated concat function
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """concatenates str1 and str2"""
+    return str1 + str2
