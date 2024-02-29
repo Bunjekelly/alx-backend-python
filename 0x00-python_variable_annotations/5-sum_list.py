f1773452edc69bcacaa472b401c33cace60744ae
Bunje Kelly <kellybunje@gmail.com>
2024-02-29 20:17:02 +0000

    sum ilist

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+This module contains an annotated sum_list function
+"""
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    A type-annotated function sum_list which takes a list input_list of floats
+    as argument and returns their sum as a float
+    """
+    return sum(input_list)
